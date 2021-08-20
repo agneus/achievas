f3f04b79dbe9349b136b19f76d5a949e1c5bd04c
Abrar Rakin <63086922+abrar-rakin@users.noreply.github.com>
2021-08-20 01:08:07 -0400

    Add files via upload

---
+++ src/components/Membership.js
@@ -0,0 +1,41 @@
+import './Membership.css';
+import Header from './Header';
+import Member from './Member';
+import FAQ from './faq';
+import Footer from './Footer';
+
+
+function Membership() {
+  return (
+    <div>
+      <Header/>
+      <div className="Membership">
+      <h1>Memberships</h1>
+      <p>description</p>
+      <div className="types">
+        <Member title="Trial Membership" 
+        subtitle = "Free"
+        access = "Limited access for 14 days"
+        ></Member>
+        <Member title="Trial Membership" 
+        subtitle = "Free"
+        access = "Limited access for 14 days"
+        ></Member>
+        <Member title="Trial Membership" 
+        subtitle = "Free"
+        access = "Limited access for 14 days"
+        ></Member>
+      </div>
+      <h1>Frequently Asked Questions</h1>
+      <div className="questions">
+        <FAQ question="What is life?" answer="Bhallagena bhai ar"></FAQ>
+        <FAQ question="What is life?" answer="Bhallagena bhai ar"></FAQ>
+        <FAQ question="What is life?" answer="Bhallagena bhai ar"></FAQ>
+      </div>
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Membership;
