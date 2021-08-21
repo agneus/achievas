f8c7650941deace0cf5a9f6e99715e288b74f3ae
Abrar Rakin <abrar@Abrars-MacBook-Air.local>
2021-08-21 04:04:15 -0400

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Signup from "./Signup"
-import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Dashboard from "./Dashboard"
+import Account from "./account"
 import Membership from "./Membership"
 import Login from "./Login"
 import PrivateRoute from "./PrivateRoute"
 import ForgotPassword from "./ForgotPassword"
-import UpdateProfile from "./UpdateProfile"
+//import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
@@ -19,10 +19,13 @@ function App() {
           <AuthProvider>
             <Switch>
               <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/account">
+              <Account />
+            </PrivateRoute>
               <PrivateRoute path="/memberships">
               <Membership />
             </PrivateRoute>
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              {/* <PrivateRoute path="/update-profile" component={UpdateProfile} /> */}
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
               <Route path="/forgot-password" component={ForgotPassword} />
