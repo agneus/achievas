05fbca08cd0208fe69254bdfe9d9d207c056ac2f
Hamza Yusuff <hbyusuff@uwaterloo.ca>
2021-08-21 12:55:08 -0400

    css done

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,8 @@ import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
+// import SignUpImage from "./signupImage.gif"
+import './Signup.css'
 
 export default function Signup() {
   const emailRef = useRef()
@@ -33,32 +35,34 @@ export default function Signup() {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
+    <div className="signupPage">
+    <div className="signupContainer">
+      <div className="signupForm">
+        <h2 className="text-center mb-4" className="signupTitle">Sign Up.</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control type="password" ref={passwordConfirmRef} required />
+              </Form.Group>
+              <button disabled={loading} className="signupBtn" type="submit">
+                Sign Up
+              </button>
+            </Form>
+          <div className="w-100 text-center mt-2">
+          Already have an account? <Link to="/login" className="logLink">Log In</Link>
+          </div>
       </div>
+    </div>
+    </div>
     </>
   )
 }
